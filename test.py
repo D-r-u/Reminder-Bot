139fb9cc32a70d2d19c3c544375e93b9c62b9289
Drupad <drupadmdas@gmail.com>
2024-07-23 19:59:36 +0530

    update test

--- test.py
+++ test.py
@@ -56,10 +56,8 @@ f32 = tk.Frame(f3)
 f33 = tk.Frame(f3)
 
 file_label = tk.Label(f31, text="File:", font=("Helvetica", 12), bg="#f0f0f0", fg="#333")
-lab3 = tk.Label(f1, text="label3")
 
 file_label.pack(side=tk.LEFT,fill='x')
-lab3.pack(fill='x')
 
 lab4 = tk.Label(f21,text="label4")
 text2 = tk.Text(f21,width=20,height=10)
